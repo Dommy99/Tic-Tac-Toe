c86c77edc161211c45ece817e2ccbf0a68d88296
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-03-29 10:58:25 -0500

    html css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("tik-tac-toe");
+//make the html
+// Title at the top of the page centered
+// player 1&2 plus tie box
+// 3x3 board
+// new game button under
+
+// make css
+// add background color
+// board color
+// styles for x and o
+// styles for new game button
+
+// js
+//target the board and all 9 blocks
+
+// first click is x player
+// add click events for all 9 blocks, and new game button
+// add logic for game
+// cant click the same block twice
+// when user clicks block its placed
+// when 3 in a row game is finished
+// save the data of who won
+// append the data to the score board
+
+// extra save the wins to local store 
