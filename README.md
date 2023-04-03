ae101d4e1dcd5451d76b1a27046aad6ceffe3c7e
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-02 22:27:56 -0500

    clean-readme

---
+++ README.md
@@ -0,0 +1,68 @@
+# Tic Tac Toe
+
+![HTML](https://img.shields.io/badge/HTML-%23dd4b25?logo=html5&style=plastic)
+![CSS](https://img.shields.io/badge/CSS-%23146eb0?logo=css3&style=plastic)
+![JAVASCRIPT](https://img.shields.io/badge/JavaScript-%23e9d44d?logo=javascript&style=plastic)
+
+## Table of Content
+
+- [Description](#description)
+- [User-Story](#User-Story)
+- [Roles](#roles)
+  - [Frontend](#frontend)
+- [Deployment](#deployment)
+  - [Demo](#demo)
+  - [Clone](#clone)
+- [Features](#Features)
+   - [Hurdles](#Hurdles)
+
+## Description
+Got some free time? Enjoy a fun game of Tic Tac Toe.
+
+## User-Story
+- As a user, I should be able to start a new tic tac toe game
+- As a user, I should be able to click on a square to add X first and then O, and so on
+- As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
+- As a user, I should not be able to click the same square twice
+- As a user, I should be shown a message when I win, lose or tie
+- As a user, I should not be able to continue playing once I win, lose, or tie
+- As a user, I should be able to play the game again without refreshing the page
+
+![Wireframe.](/wireframtictactoe.JPG)
+![TicTacToe.](/TicTacToegame.JPG)
+
+## Roles
+### Frontend
+- Dominique Akers
+CSS and HTML
+
+### BackEnd
+- Dominique Akers
+Javascript and Localstorage
+
+### Demo
+You can access a live version at [TicTacToeApp](https://dommy99.github.io/Tic-Tac-Toe/)
+
+### Clone
+1. Head to the [GitHub Repo](https://github.com/Dommy99/Tic-Tac-Toe)
+2. Click the green code.
+3. Click the copy button.
+4. Go to Terminal apply the following command
+```bash
+git clone "add clone text here"
+```
+
+## Features
+- Save data with localstorage. 
+- Responsive UI so you can enjoy on the go.
+- Player v Player local gameplay.
+- Keep track of multiple game rounds with a win, lose and tie counter.
+### Future Features
+- Audio.
+- Player v AI.
+- Icons.
+## Hurdles
+- Getting the blocks to display and take away styles at the correct time.
+- Localstorage integration and appending data.
+- Game logic (getting the game to recognize winning combinations).
+- CSS responsivness. 
--- js/script.js
+++ js/script.js
@@ -1,91 +1,71 @@
-//make the html
-// Title at the top of the page centered
-// player 1&2 plus tie box
-// 3x3 board
-// new game button under
-
-// make css
-// add background color
-// board color
-// styles for x and o
-// styles for new game button
-//add google fonts
-
-// js
-//target the board and all 9 blocks
-
-// first click is x player
-// add click events for all 9 blocks, and new game button
-// add logic for game
-// cant click the same block twice
-// when user clicks block its placed
-// when 3 in a row game is finished
-// save the data of who won
-// append the data to the score board
-
-// extra save the wins to local store
-
-// textcontent for the modal
-const playerOneWinModal = document.querySelector(
-  ".modal-content .player-one-wins"
-);
-const playerTwoWinModal = document.querySelector(
-  ".modal-content .player-two-wins"
-);
+// Selectors
+//modal start
+const playerOneWinModal = document.querySelector(".modal-content .player-one-wins");
+const playerTwoWinModal = document.querySelector(".modal-content .player-two-wins");
 const tieModal = document.querySelector(".modal-content .tie");
-
-//player color box
 const playerOneIndicator = document.querySelector(".player-one");
 const playerTwoIndicator = document.querySelector(".player-two");
+const statusModal = document.createElement("p");
+const modalContent = document.querySelector(".modal-content");
+const modal = document.getElementById("myModal");
+const openModalBtn = document.getElementById("myBtn");
+const closeModalBtn = document.getElementsByClassName("close")[0];
+// modal end
+// select all blocks on the board
+const blocks = document.querySelectorAll(".block");
+const newGameButton = document.querySelector(".new-game-button");
 
-// Retrieve data from localStorage
-const storedPlayerOneWins = parseInt(localStorage.getItem("player1Wins")) || 0;// if the local storage is null set to zero
+
+// Local storage data
+const storedPlayerOneWins = parseInt(localStorage.getItem("player1Wins")) || 0;
 const storedPlayerTwoWins = parseInt(localStorage.getItem("player2Wins")) || 0;
 const storedTies = parseInt(localStorage.getItem("tie")) || 0;
 
-// Update the text content of the elements
-document.querySelector(".player-one-wins").textContent = `Player 1 Win(s):${storedPlayerOneWins}`;
-document.querySelector(".player-two-wins").textContent = `Player 2 Win(s):${storedPlayerTwoWins}`;
-document.querySelector(".tie").textContent = `Tie:${storedTies}`;
-
-
-// Declare the current player variable, with 1 representing Player 1
+// Initialize variables
 let currentPlayer = 1;
 let playerOneWin = 0;
 let playerTwoWin = 0;
 let tie = 0;
 
-// adds Player1/2 or tie to the modal
-const statusModal = document.createElement("p");
+// Modal configuration
 statusModal.classList.add("status");
-const modalContent = document.querySelector(".modal-content");
 modalContent.appendChild(statusModal);
 
+// Initial state setup
+updateWinCounts();
 
-// Function to start a new game
+// Function declarations
+function updateWinCounts() {
+  playerOneWinModal.textContent = `Player 1 Win(s):${storedPlayerOneWins}`;
+  playerTwoWinModal.textContent = `Player 2 Win(s):${storedPlayerTwoWins}`;
+  tieModal.textContent = `Tie:${storedTies}`;
+}
+
+// resets all the game funtions to the starting state
 function newGame() {
-  // Select all block elements and store them in a constant
-  const blocks = document.querySelectorAll(".block");
-  //resets the player color box
+  resetPlayerColorBox();
+  resetModal();
+  clearBlockColors();
+  currentPlayer = 1;
+}
+
+function resetPlayerColorBox() {
   playerTwoIndicator.classList.add("player-two");
   playerOneIndicator.classList.add("player-one");
-  //resets modal
-  statusModal.textContent = ''
-  // Iterate over each block element
+}
+
+function resetModal() {
+  statusModal.textContent = '';
+}
+
+function clearBlockColors() {
   blocks.forEach((block) => {
-    // Clear the background color of each block
     block.style.backgroundColor = "";
   });
-
-  // Reset the current player to Player 1
-  currentPlayer = 1;
 }
 
 // Function to check for a win
 function checkWin() {
-  // Select all block elements and store them in a constant
-  const blocks = document.querySelectorAll(".block");
-
   // Define the possible winning combinations for a 3x3 tic-tac-toe board
   const winningCombinations = [
     [0, 1, 2],
@@ -102,7 +82,7 @@ function checkWin() {
 
   // Iterate over each winning combination
   for (const combination of winningCombinations) {
-    // Extract the three indices (a, b, and c) from the current winning combination
+    // Extract (a, b, and c) from the current winning combination
     const [a, b, c] = combination;
 
     // Check if the current winning combination is present on the board
@@ -114,13 +94,14 @@ function checkWin() {
       return true; // winning combination
     }
   }
-
+//If the block has a background color, it means that a player has already marked it
   blocks.forEach((block) => {
     if (block.style.backgroundColor) {
+      //increment by one
       clickedBlocks++;
     }
   });
-
+// If a wining combo is not found,and all the blocks are clicked = tie
   if (clickedBlocks === blocks.length) {
     tie++;
     modal.style.display = "block";
@@ -146,7 +127,7 @@ function handleClick(event) {
     return;
   }
 
-  // Get the background colors for Player 1 and Player 2 from the CSS custom properties
+  // Get the background colors for Player 1 and Player 2 from CSS
   const playerOneColor = getComputedStyle(document.documentElement)
     .getPropertyValue("--playerOne")
     .trim();
@@ -168,22 +149,18 @@ function handleClick(event) {
   }
   
 
-  // Check if there's a win and, if so, show an alert and start a new game
+  // Check if there's a win and, if so, show a modal and start a new game
   if (checkWin()) {
     if (currentPlayer === 1) {
       playerTwoIndicator.classList.add("player-two");
       playerOneIndicator.classList.remove("player-one");
       currentPlayer = 2;
       playerTwoWin++;
+      // displays the modal after a win
       modal.style.display = "block";
-    //   const player2 = parseInt(localStorage.getItem('player2Wins')) ;
+
       playerTwoWinModal.textContent = `Player 2 Win(s):${playerTwoWin}`;
       statusModal.textContent = 'Player 2 Wins!'; 
-
-    //   localStorage.setItem('currentplayer',currentPlayer);
-    //   localStorage.setItem('player2Wins',playerTwoWin);
-      
-    //   console.log(typeof(playerTwoWin + player2));
       
     } else {
       playerTwoIndicator.classList.remove("player-two");
@@ -205,29 +182,19 @@ function handleClick(event) {
   
 }
 
-// Add click event listeners to each block
-document.querySelectorAll(".block").forEach((block) => {
+// Event listeners
+blocks.forEach((block) => {
   block.addEventListener("click", handleClick);
 });
 
+newGameButton.addEventListener("click", newGame);
+
 // https://www.w3schools.com/howto/howto_css_modals.asp
-document.querySelector(".new-game-button").addEventListener("click", newGame);
-
-let modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
+openModalBtn.onclick = function () {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+closeModalBtn.onclick = function () {
   modal.style.display = "none";
 };
 
@@ -237,6 +204,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-
-// add a delay
-//  If i cant find 3 in a row for each player the its a tie
